882c726d8595e2080f4f0c4d44a64f7bc92bd52a
Joshy Orndorff <admin@joshyorndorff.com>
2019-10-23 15:14:33 -0400

    Module to own crate

--- module/src/lib.rs
+++ module/src/lib.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
 /// A runtime module that interprets a sipmlified pi calculus
 
 
--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -61,8 +61,6 @@ pub type Hash = primitives::H256;
 /// Digest item type.
 pub type DigestItem = generic::DigestItem<Hash>;
 
-mod pcalc;
-
 /// Opaque types. These are used by the CLI to instantiate machinery that don't need to know
 /// the specifics of the runtime. They can then be made to be agnostic over specific formats
 /// of data like extrinsics, allowing for them to continue syncing the network through upgrades
